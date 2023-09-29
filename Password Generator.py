026d53db9436d7401c18dd9d2dc4e0fe1ed79758
kashishpunjani <143514452+kashishpunjani@users.noreply.github.com>
2023-09-29 21:42:18 +0530

    Add files via upload
    A password generator is a useful tool that generates strong and random passwords for users. This project aims to create a password generator application using Python, allowing users to specify the length and complexity of the password. and this it prompt the user for length of password  and then generate the password using different combination of characters and symbols and print the password .

---
+++ Password Generator.py
@@ -0,0 +1,45 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      kashi
+#
+# Created:     29-09-2023
+# Copyright:   (c) kashi 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+# final task of random generated password
+import string,secrets,random,array
+#new..........................
+MAX_lEN = int(input('enter the desired length of the password : '))
+no = string.digits
+upp = string.ascii_uppercase
+low = string.ascii_lowercase
+sym = string.punctuation
+total = no + upp + low + sym
+# by using combination we get 26 lower + 26 uper + 10 digit + 32 symbol = 94 length
+
+
+#1 leter from each = 4
+rand_upper = random.choice(upp)
+rand_lower = random.choice(low)
+rand_digit = random.choice(no)
+rand_symbol = random.choice(sym)
+
+# 4 character
+temp = rand_upper+ rand_lower +rand_digit +rand_symbol
+
+#total = 94 , temp = 4  maxlenght = 12
+
+for x in range(MAX_lEN - 4):             # 12 - 4 = 7
+    temp = temp + random.choice(total)   # temp = 4 # total = 94 but due to random it take 1 no. ---->temp = 4+1  => 5
+    temp_pass_list = array.array('u', temp)  # u is use for unicode character here temp_pass_list gives random 12 character
+    random.shuffle(temp_pass_list) # range of x is 7
+password = ''
+for x in temp_pass_list:
+    password = password+ x
+
+print(password)
+
