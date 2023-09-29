47473a4f1d45f05cb4c2507cc9844072a01a668e
kashishpunjani <143514452+kashishpunjani@users.noreply.github.com>
2023-09-29 20:57:40 +0530

    Add files via upload
    Here I'll create rock paper scissor game in python . In this Prompt the user to choose rock, paper, or scissors and using import random Generate a random choice (rock, paper, or scissors) for the computer .
    Game logic - Determine the winner based on the user's choice and the computer's choice. Rock beats scissors, scissors beat paper, and paper beats rock.  
    score - win score  = 2, loose score  = 0, tie score = 1  if user win user get win score + tie score and if computer win computer get win score + tie score .
    if user want to play again select yes otherwise select no . 
    here user also give feedback .
    it is based on command line

---
+++ rock_paper_scissor.py
@@ -0,0 +1,90 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      kashi
+#
+# Created:     29-09-2023
+# Copyright:   (c) kashi 2023
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+import random
+print('Rock beats scissors, scissors beat paper, and paper beats rock, win score  = 2, loose score  = 0, tie score = 1')
+while True:
+    user = input("enter the choice (rock,paper,scissor): ")
+    rand = ('rock' ,'paper','scissor')
+    computer = random.choice(rand)
+    print('computer choice is : ',computer)
+
+    user_win = 0
+    computer_win = 0
+
+    if user == computer :
+        print('its tie')
+        print('score---->')
+        user_win+=1
+        computer_win+=1
+        print("it's tie user score = ",user_win)
+        print("it's tie computer score = ",user_win)
+
+    elif user == 'rock' :
+        if computer == 'scissor':
+            print('user win')
+            print('score---->')
+            user_win+=2
+            print('user score = ',user_win)
+            print('computer score = ',computer_win)
+        else:
+            print('computer win')
+            print('score---->')
+            computer_win+=2
+            print('user score = ',user_win)
+            print('computer score = ',computer_win)
+
+
+    elif user == 'paper' :
+        if computer == 'rock':
+            print('user win')
+            print('score---->')
+            user_win+=2
+            print('user score = ',user_win)
+            print('computer score = ',computer_win)
+        else:
+            print('computer win')
+            print('score---->')
+            computer_win+=2
+            print('user score = ',user_win)
+            print('computer score = ',computer_win)
+
+    elif user == 'scissor' :
+        if computer == 'paper':
+            print('user win')
+            print('score---->')
+            user_win+=2
+            print('user score = ',user_win)
+            print('computer score = ',computer_win)
+        else:
+            print('computer win')
+            print('score---->')
+            computer_win+=2
+            print('user score = ',user_win)
+            print('computer score = ',computer_win)
+
+
+
+    else:
+        print('enter valid choice')
+
+
+
+    game = input('want to play again? (y/n)  : ')
+    if game.lower() == 'n':
+        break
+
+feedback = input('want to give feedback? (yes/no)  : ')
+if feedback == 'yes':
+    x = input('Please enter your feedback!')
+else:
+    print('')
+
